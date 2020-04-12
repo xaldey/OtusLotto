4ffd69c81aa0cd415186af2440c2c3f9725c3b66
xaldey <fguzkp25@gmail.com>
2020-04-12 16:30:41 +0300

    Добавил код

---
+++ game_stuff.py
@@ -0,0 +1,36 @@
+from random import random, uniform, randrange
+
+card_numbers = set()
+# for num in randrange(100):
+#     while len(card_numbers) < 15:
+#         print(num)
+#         card_numbers.
+print(type(card_numbers))
+
+while len(card_numbers) < 15:
+    num = randrange(100)
+    card_numbers.add(num)
+
+print(len(card_numbers))
+print(card_numbers)
+
+
+
+#
+# class Card():
+#     pass
+#
+# class Player():
+#     pass
+#
+# class Bag():
+#     pass
+#
+# class Barrel():
+#     pass
+
+#
+# if __name__ == '__main__':
+#     main
+#
+#
